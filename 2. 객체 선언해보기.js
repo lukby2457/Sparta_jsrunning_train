57b4d2782235e44c360b335e0c50e8d2fb23f266
lukby2457 <kjh7442455@gmail.com>
2024-07-24 11:56:26 +0900

    2번 출력 부분 수정

--- 2. 객체 선언해보기.js
+++ 2. 객체 선언해보기.js
@@ -4,6 +4,6 @@ const junhyun = {
   mbti: "ISTJ"
 };
 
-console.log('name : ' + junhyun.name);
+console.log('name :', junhyun.name);
 console.log("age : " + junhyun.age);
 console.log(`MBTI : ${junhyun.mbti}`);
